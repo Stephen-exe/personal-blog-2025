136d9fdffbb004165001c5a2ef99a40ee36cbdd8
Stephen-exe <watchstephencreate@gmail.com>
2025-08-11 15:00:31 -0400

    logic.js updating

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,33 @@
-// TODO: Create logic to toggle the light/dark mode styles for the page and circle. The mode should be saved to local storage.
+// TODO: Create logic to toggle the light/dark mode styles for the page and circle. 
+const lightAndDark = function (mode) {
+  let icon, circleColor;
 
+  if (mode === 'light') {
+    icon = '☀️';
+    circleColor = '#ffd000ff';
+  } else {
+    icon = '🌒';
+    circleColor = '#9981bdff';
+  }
+  modeBtnEl.textContent = icon;
+  document.body.classList = mode;
+  document.documentElement.style.setProperty('--circle-color', circleColor);
+}
+//The mode should be saved to local storage.
+const = whatModeIsIt = function () {
+  const mode = getMode();
+
+  let nextMode;
+
+  if (mode === 'light') {
+    nextMode = 'dark';
+  } else {
+    nextMode = 'light';
+  }
+
+  lightAndDark(nextMode);
+
+}
 
 // TODO: Create a function called `readLocalStorage` that reads from local storage and returns the data. If no data exists, return an empty array.
 

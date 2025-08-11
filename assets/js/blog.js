d03e19b1ba0f61654294e357cbaff0b216314cbf
Stephen-exe <watchstephencreate@gmail.com>
2025-08-11 13:48:55 -0400

    updated blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -3,10 +3,20 @@ const mainEl = document.querySelector('main');
 const backBtnEl = document.querySelector('back');
 
 // TODO: Create a function that builds an element and appends it to the DOM
+const buildElement = function (type, text, parent) {
+  const tag = document.createElement(type);
+  tag.textContent = text;
+  parent.appendChild(tag);
 
-
+  return tag;
+}
 // TODO: Create a function that handles the case where there are no blog posts to display
+const ifEmpty = function () {
+  buildElement('h2', 'Nothing has been posted..', mainEl);
+  const a = buildElement('a', 'Write something down..', mainEl);
 
+  a.href = '.index.html';
+};
 // TODO: Create a function called `renderBlogList` that renders the list of blog posts if they exist. If not, call the no posts function.
 
 // TODO: Call the `renderBlogList` function
